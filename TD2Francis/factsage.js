6e34872e73c8a11d0df556be719b818db156b35f
Francis <francis.morin-leblanc@polymtl.ca>
2019-02-17 13:06:17 -0500

    FML - ON RECOIS LES DONNES

--- TD2Francis/factsage.js
+++ TD2Francis/factsage.js
@@ -8,22 +8,39 @@ $( "form" ).submit(function( event ) {
 function postData(form) {
     fetch("http://localhost:8080", {
         method: 'POST',
-        body: form,
+        body: form
     })
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log(JSON.stringify(myJson));
-    })
+    // À noter, la fonction .json() provoquait une SyntaxError
+    // Cette erreur semblait être causée par l'absence de guillemets autour des éléments de data-output.json.
+    // Nous avons donc pris la liberté de modifier le fichier en ajoutant des guillemets, faute d'avoir
+    // une autre solution évidente sous la main.
+    .then((res)=>res.json())
+    .then(data=> {console.log(data);
+                   console.log(data['output1'][3]['libelle']); 
+                formatData(data);})
+   
     .catch(function(error) {
         console.log(error);
     })
 }
 
+function formatData(data) {
+    var x = document.querySelectorAll("#output1 td");
+    for (var i = 0; i < data['output1'].length; i++){
+        x[i*2].innerHTML = data['output1'][i]['libelle'];
+        x[i*2 + 1].innerHTML = data['output1'][i]['concentration'];
+    }
+}
 
 
-
+/*
+fetch("data-output.json")
+.then(response => {
+    response.json();
+}).then(data => {
+    console.log(data);
+})
+*/
 /*
 (event) => {
     event.preventDefault();
