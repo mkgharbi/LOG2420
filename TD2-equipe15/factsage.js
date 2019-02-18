4d369df61580314c646363527ca16d012a68ff20
Gharbi Mohamed Khairallah <lmkg97@gmail.com>
2019-02-18 01:36:13 -0500

    MKG : reste que l'ergonomie

--- TD2-equipe15/factsage.js
+++ TD2-equipe15/factsage.js
@@ -16,41 +16,47 @@ function default_values() {
 }
 
 //Partie server : 
-
-
-$("form").submit(function() {
+$("form").submit(function(event) {
     event.preventDefault();
     postData($(this).serialize());
-})
+    document.getElementById("loader").style.display = "block";
 
+});
 
 function postData(form) {
-    setTimeout(document.getElementById("loading-bar").style.display = "block", 3000);
     fetch("http://localhost:8080", {
-            method: "POST",
+            method: 'POST',
             body: form
         })
         // À noter, la fonction .json() provoquait une SyntaxError
-        // Cette erreur semblait être causée par l"absence de guillemets autour des éléments de data-output.json.
-        // Nous avons donc pris la liberté de modifier le fichier en ajoutant des guillemets, faute d"avoir
+        // Cette erreur semblait être causée par l'absence de guillemets autour des éléments de data-output.json.
+        // Nous avons donc pris la liberté de modifier le fichier en ajoutant des guillemets, faute d'avoir
         // une autre solution évidente sous la main.
-        .then((response) => response.json())
+        .then((res) => res.json())
         .then(data => {
             console.log(data);
-            console.log(data["output1"][3]["libelle"]);
+            console.log(data['output1'][3]['libelle']);
             formatData(data);
         })
-        .catch(error => console.error(error))
+
+    .catch(function(error) {
+        console.log(error);
+    })
 }
 
 function formatData(data) {
-    document.getElementById("loading-bar").style.display = "none";
-    document.getElementById("output1").style.display = "block";
+    document.getElementById("output1").style.display = "table";
+    document.getElementById("output2").style.display = "table";
+    document.getElementById("loader").style.display = "none";
     var x = document.querySelectorAll("#output1 td");
-    x[0].innerHTML = "Libelle";
-    x[1].innerHTML = "Concentration";
-    for (var i = 0; i < data["output1"].length; i++) {
-        x[i * 2 + 2].innerHTML = data["output1"][i]["libelle"];
-        x[i * 2 + 3].innerHTML = data["output1"][i]["concentration"];
+    for (let i = 0; i < data['output1'].length; i++) {
+        x[i * 2].innerHTML = data['output1'][i]['libelle'];
+        x[i * 2 + 1].innerHTML = data['output1'][i]['concentration'];
+    }
+    var y = document.querySelectorAll("#output2 td");
+    for (let i = 0; i < data['output2'].length; i++) {
+        y[i * 3].innerHTML = data['output2'][i]['libelle'];
+        y[i * 3 + 1].innerHTML = data['output2'][i]['unite'];
+        y[i * 3 + 2].innerHTML = data['output2'][i]['valeur'];
     }
 }
