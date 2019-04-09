4d70ef94daadef6c2dd3e41c3538441cb8aa5c0b
Gharbi Mohamed Khairallah <lmkg97@gmail.com>
2019-04-09 18:11:05 -0400

    MKG : some changes

---
+++ TD3-equipe15/connectionHandler.js
@@ -0,0 +1,51 @@
+/*
+class connectionHandler {
+    constructor(username) {
+        this.username = username;
+    }
+
+    ws = new WebSocket(`ws://log2420-nginx.info.polymtl.ca/chatservice?username=${model.username}`);
+    ws.onmessage 
+}
+*/
+
+var ws = new WebSocket(`ws://log2420-nginx.info.polymtl.ca/chatservice?username=${model.username}`)
+
+ws.onmessage = function(event) {
+    console.log(JSON.parse(event.data));
+    let message = JSON.parse(event.data);
+    switch (message.eventType) {
+        case "onMessage":
+            console.log("Message recu");
+            model.addMessage(message.data);
+            break;
+        case "updateChannelsList":
+            console.log("update les channels");
+            model.setChannels(message.data);
+
+            console.log(model.isActive("allo"));
+            let bobo = new Channel("allo");
+            console.log(bobo);
+            model.addActiveChannel(bobo);
+            console.log(model.isActive("allo"));
+
+            let amessage = new Message("onGetChannel", "dbf646dc-5006-4d9f-8815-fd37514818ee");
+            ws.send(JSON.stringify(amessage));
+            break;
+        case "onJoinChannel":
+            console.log("JoinChannel");
+            break;
+        case "onLeaveChannel":
+            console.log("LeaveChannel");
+            break;
+        case "onError":
+            console.log("error");
+            break;
+        case "onGetChannel":
+            console.log("getChannel");
+            console.log(message.data);
+            model.addActiveChannel(message.data);
+            break;
+
+    };
+}
---
+++ TD3-equipe15/main.js
@@ -0,0 +1,124 @@
+class ChannelView {
+    updateChannelList(channels) {
+        console.log("Update la channel list avec");
+        console.log(channels);
+        for (let i = 0; i < channels.length; i++) {
+            var para = document.createElement("div");
+            var node = document.createTextNode(channels[i].name);
+            para.setAttribute("id", channels[i].id);
+            para.appendChild(node);
+            let element = document.getElementById("dynamicChannelList");
+            element.appendChild(para);
+        }
+    }
+
+    updateCurrentChannel(channel) {
+        console.log("Change le nom channel pour");
+        console.log(channel.name);
+
+    }
+
+    addChannel(channel) {
+
+    }
+}
+
+class MessageView {
+    loadChannel(channel) {
+        console.log("channelALLO")
+    }
+
+    addMessage(message) {
+
+    }
+
+}
+
+class View {
+    channelView = new ChannelView();
+    messageView = new MessageView();
+}
+var view = new View();
+
+
+class Model {
+    firstConnexion = true;
+    userName = "Default";
+    channels = [];
+    currentChannel;
+    activeChannels = [];
+
+    setChannels(channels) {
+        this.channels = channels;
+        if (this.firstConnexion == true) {
+            this.setCurrentChannel(channels[0]);
+            this.firstConnexion = false;
+        }
+        view.channelView.updateChannelList(channels);
+    }
+
+    addActiveChannel(channel) {
+        if (this.isActive(channel.id) == false) {
+            this.activeChannels.push(channel);
+        }
+    }
+
+    isActive(id) {
+        for (let i = 0; i < this.activeChannels.length; i++) {
+            if (this.activeChannels[i].id == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    getIndexActiveChannel(id) {
+        for (let i = 0; i < this.activeChannels.length; i++) {
+            if (this.activeChannels[i].id == id) {
+                return i;
+            }
+        }
+    }
+
+    setCurrentChannel(channel) {
+        this.currentChannel = channel;
+        view.channelView.updateCurrentChannel(channel);
+        view.messageView.loadChannel(channel);
+    }
+
+    addMessage(message) {
+        if (this.isActive(message.channelId) == true) {
+            let index = getIndexActiveChannel(message.channelId);
+            this.activeChannels[index].messages.push(message);
+
+            if (this.currentChannel.id == message.channelId) {
+                view.messageView.addMessage(message);
+            }
+
+        }
+    }
+
+
+}
+
+var model = new Model();
+
+
+
+/*
+class Controller {
+
+}
+
+
+
+
+
+class channelView {
+
+}
+
+class messageView {
+
+}
+*/
