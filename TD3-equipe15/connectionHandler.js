9c96cf9fb7cc3a937abb97ac24333a29ede4ffa7
Gharbi Mohamed Khairallah <lmkg97@gmail.com>
2019-04-09 22:14:47 -0400

    MKG : ajout dans html

--- TD3-equipe15/connectionHandler.js
+++ TD3-equipe15/connectionHandler.js
@@ -22,15 +22,25 @@ ws.onmessage = function(event) {
         case "updateChannelsList":
             console.log("update les channels");
             model.setChannels(message.data);
-
+            /*
             console.log(model.isActive("allo"));
             let bobo = new Channel("allo");
             console.log(bobo);
             model.addActiveChannel(bobo);
             console.log(model.isActive("allo"));
-
+            /*
+            console.log(model.isActive("allo"));
+            let bobo = new Channel("allo");
+            console.log(bobo);
+            model.addActiveChannel(bobo);
+            console.log(model.isActive("allo"));
+            
             let amessage = new Message("onGetChannel", "dbf646dc-5006-4d9f-8815-fd37514818ee");
             ws.send(JSON.stringify(amessage));
+            
+            let amessage = new Message("onGetChannel", "dbf646dc-5006-4d9f-8815-fd37514818ee");
+            ws.send(JSON.stringify(amessage)); 
+            */
             break;
         case "onJoinChannel":
             console.log("JoinChannel");
@@ -48,4 +58,14 @@ ws.onmessage = function(event) {
             break;
 
     };
+}
+
+function sendJoinRequest(channelId, user) {
+    let joinRequest = new Message("onJoinChannel", channelId, null, user);
+    ws.send(JSON.stringify(joinRequest));
+}
+
+function sendLeaveRequest(channelId, user) {
+    let leaveRequest = new Message("onLeaveChannel", channelId);
+    ws.send(JSON.stringify(leaveRequest));
 }
